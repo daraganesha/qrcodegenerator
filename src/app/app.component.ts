e570f1b85edee65a9ce762c81107fe569a53e310
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-08-30 14:27:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'qrcodegenerator';
+  imageUrl: string = '';
+  message: string = '';
+  size: number;
+  text: string = '';
+  format: string = '';
+  bgcolor: string = '';
+  color: string = '';
+  isGenerated: boolean = false;
+
+  constructor() {}
+
+  buttonGenerate() {
+    if (this.text === '') {
+      this.message = 'Please enter the text';
+      this.isGenerated = false;
+    } else if (this.size < 50 || this.size > 1000 || isNaN(this.size)) {
+      this.message = 'Size should be number between 50 to 1000';
+      this.isGenerated = false;
+    } else {
+      this.imageUrl =
+        'http://api.qrserver.com/v1/create-qr-code/?size=' +
+        this.size +
+        'x' +
+        this.size +
+        '&data=' +
+        this.text +
+        '&format=' +
+        this.format +
+        '&bgcolor=' +
+        this.bgcolor +
+        '&color=' +
+        this.color;
+      this.message = '';
+      this.isGenerated = true;
+    }
+  }
 }
