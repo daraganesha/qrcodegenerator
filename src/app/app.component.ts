63945eb2e38f363bafddcb04f094c9b174e294f2
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-09-02 16:34:42 +0200

    commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  imageUrl: string = '';
+  imageViewURL: string = ''; //URL von dem zu zeigenden Bild, es hat Max Size 300px.
+  imageDownloadURL: string = '';
+  errorMessage: string = '';
   message: string = '';
-  size: number;
+  sizeAsText = '';
+  generatedSizeAsText = '';
+  size: number = 0;
   text: string = '';
-  format: string = '';
+  generatedText: string = '';
+  format: string = 'jpeg';
+  generatedFormat: string = '';
   bgcolor: string = '';
   color: string = '';
   isGenerated: boolean = false;
@@ -22,38 +28,118 @@ export class AppComponent {
     this.format = f;
   }
 
-  buttonGenerate() {
-    if (this.text === '') {
-      this.message = 'Please enter the text';
-      this.isGenerated = false;
-    } else if (this.size < 50 || this.size > 1000 || isNaN(this.size)) {
-      this.message = 'Size should be number between 50 to 1000';
-      this.isGenerated = false;
-    } else {
-      this.imageUrl =
-        'http://api.qrserver.com/v1/create-qr-code/?size=' +
-        this.size +
-        'x' +
-        this.size +
-        '&data=' +
-        this.text +
-        '&format=' +
-        this.format +
-        '&bgcolor=' +
-        this.bgcolor +
-        '&color=' +
-        this.color;
-      this.message = '';
+  setSize(s: string) {
+    this.sizeAsText = s;
+  }
+
+  getUrl(
+    size: number,
+    text: string,
+    format: string,
+    bgcolor: string,
+    color: string
+  ) {
+    var url: string;
+    url =
+      'http://api.qrserver.com/v1/create-qr-code/?size=' +
+      size +
+      'x' +
+      size +
+      '&data=' +
+      text +
+      '&format=' +
+      format +
+      '&bgcolor=' +
+      bgcolor +
+      '&color=' +
+      color;
+    return url;
+  }
+  /* Setze die Werte der URLs, falls die Bedingungen stimmen  */
+  generateButton() {
+    this.size = parseInt(this.sizeAsText);
+    this.generatedSizeAsText = this.sizeAsText;
+    this.generatedText = this.text;
+    this.generatedFormat = this.format;
+    this.imageViewURL = '';
+    this.imageDownloadURL = '';
+    this.message = '';
+    this.errorMessage = '';
+    /*Format zum Attribute color in <input> ist mit #, in URL ohne*/
+    if (this.bgcolor != '') this.bgcolor = this.bgcolor.substring(1);
+    if (this.color != '') this.color = this.color.substring(1);
+    if (this.text === '') this.errorMessage += 'Please enter the text. '; //falsche Eingabe: kein Text
+    if (this.size < 50 || this.size > 1000 || isNaN(this.size))
+      //falsche Eingabe: Größe falsch oder nicht im angegebenen Zahlenraum
+      this.errorMessage += 'Size should be numbers between 50 and 1000';
+    if (this.text != '' && this.size >= 50 && this.size <= 1000) {
+      this.errorMessage = '';
+      this.imageDownloadURL = this.getUrl(
+        this.size,
+        this.text,
+        this.format,
+        this.bgcolor,
+        this.color
+      );
+      /*Bei angeforderten Größen von mehr als 300x300px wird nur Bild im Größen 300x300px angezeigt. Zum Herunterladen steht das Bild im gewünschten Größe zur Verfügung */
+      if (this.size <= 300) {
+        this.imageViewURL = this.imageDownloadURL;
+      } else {
+        this.message =
+          'The QR Code is only displayed at a size of 300px but it will be downloaded at a size of ' +
+          this.size +
+          'px.';
+        this.imageViewURL = this.getUrl(
+          300,
+          this.text,
+          this.format,
+          this.bgcolor,
+          this.color
+        );
+      }
       this.isGenerated = true;
     }
   }
   downloadQRCode() {
-    this.http.get(this.imageUrl, { responseType: 'blob' }).subscribe((data) => {
-      const downloadURL = window.URL.createObjectURL(data);
-      const link = document.createElement('a');
-      link.href = downloadURL;
-      link.download = 'creativeFileName.' + this.format;
-      link.click();
-    });
+    /*Überprüfe, ob es noch Änderungen gibt vorm letzten Generieren. */
+    if (
+      this.isGenerated &&
+      this.generatedText === this.text &&
+      this.generatedFormat === this.format &&
+      this.generatedSizeAsText === this.sizeAsText
+    ) {
+      //falls nicht, dann herunterladen
+      this.http.get(this.imageDownloadURL, { responseType: 'blob' }).subscribe(
+        (data) => {
+          const link = document.createElement('a');
+          link.href = window.URL.createObjectURL(data);
+          link.download = 'QRCode' + this.text.substring(0, 10);
+          link.click();
+        },
+        (error) => {
+          this.errorMessage = 'Unknown Error from Server: ' + error.message;
+        }
+      );
+    } else {
+      //andernfalls wird nichts heruntergeladen
+      this.message = 'Some changes occur. Please generate before downloading.';
+    }
+  }
+  /*zurück zum Anfangszustand */
+  refreshButton() {
+    this.imageViewURL = '';
+    this.imageDownloadURL = '';
+    this.errorMessage = '';
+    this.message = '';
+    this.sizeAsText = '';
+    this.generatedFormat = '';
+    this.generatedSizeAsText = '';
+    this.generatedText = '';
+    this.size = 0;
+    this.text = '';
+    this.format = 'jpeg';
+    this.bgcolor = '';
+    this.color = '';
+    this.isGenerated = false;
   }
 }
