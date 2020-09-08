f84e5a0d7132b1ccb4074975fa0f6866c215021c
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-09-09 00:04:41 +0200

    color and size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   imageViewURL: string = ''; //URL von dem zu zeigenden Bild, es hat Max Size 300px.
-  imageDownloadURL: string = '';
+  imageDownloadURL: string = ''; //URL vom herunterzuladenden Bild, kann von 50px bis 1000px groß sein.
   errorMessage: string = '';
   message: string = '';
-  sizeAsText = '';
-  generatedSizeAsText = '';
-  size: number = 50;
+  sizeAsText: string = '';
+  generatedSizeAsText: string = '';
+  size: number;
   text: string = '';
   generatedText: string = '';
   format: string = 'jpeg';
   generatedFormat: string = '';
-  bgcolor: string = '';
+  bgcolor: string = '#FFFFFF';
+  bgcolorURL: string = '';
   color: string = '';
+  colorURL: string = '';
   isGenerated: boolean = false;
-  showInfo: boolean;
 
-  constructor(private http: HttpClient) {
-    this.showInfo = false;
-  }
+  constructor(private http: HttpClient) {}
 
   setFormat(f: string) {
     this.format = f;
@@ -58,51 +57,55 @@ export class AppComponent {
       color;
     return url;
   }
-  /* Setze die Werte der URLs, falls die Bedingungen stimmen  */
+
   generateButton() {
-    this.size = parseInt(this.sizeAsText);
-    this.generatedSizeAsText = this.sizeAsText;
-    this.generatedText = this.text;
-    this.generatedFormat = this.format;
     this.imageViewURL = '';
     this.imageDownloadURL = '';
     this.message = '';
     this.errorMessage = '';
-    /*Format zum Attribute color in <input> ist mit #, in URL ohne*/
-    if (this.bgcolor.includes('#')) this.bgcolor = this.bgcolor.substring(1);
-    if (this.color.includes('#')) this.color = this.color.substring(1);
     if (this.text === '') this.errorMessage += 'Please enter the text. '; //falsche Eingabe: kein Text
-    if (this.size < 50 || this.size > 1000 || isNaN(this.size))
-      //falsche Eingabe: Größe falsch oder nicht im angegebenen Zahlenraum
-      this.errorMessage += 'Size should be numbers between 50 and 1000';
-    if (this.text != '' && this.size >= 50 && this.size <= 1000) {
-      this.errorMessage = '';
-      this.imageDownloadURL = this.getUrl(
-        this.size,
-        this.text,
-        this.format,
-        this.bgcolor,
-        this.color
-      );
-      /*Bei angeforderten Größen von mehr als 300x300px wird nur Bild im Größen 300x300px angezeigt. Zum Herunterladen steht das Bild im gewünschten Größe zur Verfügung */
-      if (this.size <= 300) {
-        this.imageViewURL = this.imageDownloadURL;
-      } else {
-        this.message =
-          'The QR Code is only displayed at a size of 300px but it will be downloaded at a size of ' +
-          this.size +
-          'px.';
-        this.imageViewURL = this.getUrl(
-          300,
+    if (/^\d+$/.test(this.sizeAsText)) {
+      this.size = parseInt(this.sizeAsText);
+      this.generatedSizeAsText = this.sizeAsText;
+      this.generatedText = this.text;
+      this.generatedFormat = this.format;
+      /*Format zum Attribute color in <input> ist mit #, in URL ohne # */
+      if (this.bgcolor.includes('#'))
+        this.bgcolorURL = this.bgcolor.substring(1);
+      if (this.color.includes('#')) this.colorURL = this.color.substring(1);
+      if (this.size < 50 || this.size > 1000)
+        //falsche Eingabe: Größe falsch oder nicht im angegebenen Zahlenraum
+        this.errorMessage += 'Size should be numbers between 50 and 1000.';
+      if (this.text != '' && this.size >= 50 && this.size <= 1000) {
+        this.errorMessage = '';
+        this.imageDownloadURL = this.getUrl(
+          this.size,
           this.text,
           this.format,
-          this.bgcolor,
-          this.color
+          this.bgcolorURL,
+          this.colorURL
         );
+        /*Bei angeforderten Größen von mehr als 300x300px wird nur Bild im Größen 300x300px angezeigt. Zum Herunterladen steht das Bild im gewünschten Größe zur Verfügung */
+        if (this.size <= 300) {
+          this.imageViewURL = this.imageDownloadURL;
+        } else {
+          this.message =
+            'The QR Code is only displayed at a size of 300px but it will be downloaded at a size of ' +
+            this.size +
+            'px.';
+          this.imageViewURL = this.getUrl(
+            300,
+            this.text,
+            this.format,
+            this.bgcolorURL,
+            this.colorURL
+          );
+        }
+        this.isGenerated = true;
       }
-      this.isGenerated = true;
-    }
+    } else this.errorMessage += 'Size should be numbers between 50 and 1000';
   }
+
   downloadQRCode() {
     /*Überprüfe, ob es noch Änderungen gibt vorm letzten Generieren. */
     if (
@@ -128,6 +131,7 @@ export class AppComponent {
       this.message = 'Some changes occur. Please generate before downloading.';
     }
   }
+
   /*zurück zum Anfangszustand */
   refreshButton() {
     this.imageViewURL = '';
@@ -138,11 +142,13 @@ export class AppComponent {
     this.generatedFormat = '';
     this.generatedSizeAsText = '';
     this.generatedText = '';
-    this.size = 50;
+    this.size;
     this.text = '';
     this.format = 'jpeg';
-    this.bgcolor = '';
+    this.bgcolor = '#FFFFFF';
+    this.bgcolorURL = '';
     this.color = '';
+    this.colorURL = '';
     this.isGenerated = false;
   }
 }
